bd56c86715e998e1a8854063b25906ea20fa1631
kirill9366 <kirill9366@gmail.com>
2022-05-11 11:34:01 +0400

    add miniatures
    modified:   course_projects/views.py
    	modified:   electrician_site/settings.py
    	new file:   media/cache/01/6a/016aa3af93680b40d2f753fb0c4cde33.jpg
    	new file:   media/cache/23/ac/23ac3a2ccb2cd5f4be6aac6defda079d.jpg
    	new file:   media/cache/29/4e/294e2c9b008b87622c90d6959c421da0.jpg
    	new file:   media/cache/38/45/38453fc0f409a643256006ccc299b3ab.jpg
    	new file:   media/cache/3e/80/3e80b71a9831c29f7d28b654820b7e1a.jpg
    	new file:   media/cache/56/c0/56c048de406e6accd3eb8f0a35340577.jpg
    	new file:   media/cache/67/36/67369b752b8b8a1d7f8eb75d96763c35.jpg
    	new file:   media/cache/88/25/8825962501c45087a875db9e28ca8cdb.jpg
    	new file:   media/cache/9f/4d/9f4d3e5283a4d62f76ea33ef82d3f60d.jpg
    	new file:   media/cache/a4/23/a423bdbbe1abda1b3f409e14f4027eb7.jpg
    	new file:   media/cache/a5/8d/a58d786a18170b2cf28f3f812089bae3.jpg
    	new file:   media/cache/b6/a1/b6a1273493f5fa2d88f4a77d75e1eef2.jpg
    	new file:   media/cache/c3/b9/c3b9596e8e36ab1f849d6564ad68d3ca.jpg
    	new file:   media/course_projects/profile_photo.png
    	new file:   media/course_projects/profile_photo_eOSfFM3.png
    	modified:   templates/course_projects/course_project.html
    	modified:   templates/course_projects/course_projects.html
    	modified:   templates/students/student_page.html
    	modified:   templates/students/students.html

--- course_projects/views.py
+++ course_projects/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
 # locale imports
-from .models import CourseProjectModel
+from .models import CourseModel, CourseProjectModel
 
 
 class CourseProjectsView(ListView):
@@ -11,6 +11,16 @@ class CourseProjectsView(ListView):
     paginate_by = 9
 
     template_name = 'course_projects/course_projects.html'
+
+    def get_queryset(self):
+
+        if self.request.user.is_superuser:
+            return self.model.objects.all()
+
+        course = CourseModel.objects.get(
+            number=self.request.user.addinfo.course_num
+        )
+        return self.model.objects.filter(course=course)
 
 
 class CourseProjectDetailView(DetailView):
